2d7fe0f92b1708a168fa57025a722a98d95b8209
Tai <nguyentantaitcag2000@gmail.com>
2024-05-14 21:21:38 +0700

    test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex align-items-center flex-column" style="height: 100vh;">
-        <h1 class="text-center w-100">Home page</h1>
+        <h1 class="text-center w-100">Home page 2</h1>
         <router-link to="/contact">Contact</router-link>
     </div>
     
